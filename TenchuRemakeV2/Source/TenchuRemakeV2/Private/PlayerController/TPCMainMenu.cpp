a8a038d328d2f6f822b6b1de488c4b8ccb642f87
Sherwin Espela <sherwinjai_espela@yahoo.ca>
2024-11-22 11:41:01 -0800

    Main Menu : controls setup and vfx finetuning

--- TenchuRemakeV2/Source/TenchuRemakeV2/Private/PlayerController/TPCMainMenu.cpp
+++ TenchuRemakeV2/Source/TenchuRemakeV2/Private/PlayerController/TPCMainMenu.cpp
@@ -2,4 +2,28 @@
 
 
 #include "PlayerController/TPCMainMenu.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "Kismet/GameplayStatics.h"
 
+void ATPCMainMenu::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* PlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	PlayerSubsystem->AddMappingContext(IMCMainMenu, 0);
+}
+
+void ATPCMainMenu::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(IARestartLevel, ETriggerEvent::Triggered, this, &ATPCMainMenu::RestartLevel);
+}
+
+void ATPCMainMenu::RestartLevel()
+{
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+}
--- TenchuRemakeV2/Source/TenchuRemakeV2/Public/PlayerController/TPCMainMenu.h
+++ TenchuRemakeV2/Source/TenchuRemakeV2/Public/PlayerController/TPCMainMenu.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "TPCMainMenu.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+
 /**
  * 
  */
@@ -14,4 +17,27 @@ class TENCHUREMAKEV2_API ATPCMainMenu : public APlayerController
 {
 	GENERATED_BODY()
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+protected:
+	// Player Inputs
+	UPROPERTY(EditDefaultsOnly, Category = "Player Input")
+	UInputMappingContext* IMCMainMenu;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+	UInputAction* IARestartLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+	UInputAction* IASwitchUp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+	UInputAction* IASwitchDown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+	UInputAction* IASelectMode;
+
+protected:
+	void RestartLevel();
 };
